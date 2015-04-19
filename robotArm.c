093ae632f3fba4d05962b6ecba5c8f9c74df5c22
MeenaGuptaIwasaki <mguptaiwasaki@hotmail.com>
2015-04-19 14:23:56 -0400

    added more to robot arm file

--- robotArm.c
+++ robotArm.c
@@ -1,1 +1,10 @@
+const float L1 = 3.75;
+const float L2 = 2.5;
 
+float getGoalDegree2(float x, float y, bool sector){
+	return 0;
+}
+
+float getGoalDegree1(float x, float y, float theta2){
+	return 0;
+}
