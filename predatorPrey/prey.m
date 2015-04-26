e9f735b6eeffe0d98209788b9f73ed00214a6ef7
MeenaGuptaIwasaki <mguptaiwasaki@hotmail.com>
2015-04-26 19:46:03 -0400

    _

--- predatorPrey/prey.m
+++ predatorPrey/prey.m
@@ -1,13 +1,19 @@
 %COM_CloseNXT all
 %h = COM_OpenNXT('bluetooth.ini');
-COM_SetDefaultNXT(h)
+%COM_SetDefaultNXT(h)
 
-myId = 9; % CHANGE ME: We will tell you what your tag's id is
-enemyID = 1;
+offset = .08; %forward
+
+myId = 7; % CHANGE ME: We will tell you what your tag's id is
+enemyID = 7;
 
 HPS = HowiePositioningSystem;
 
 ids = HPS.getVisibleIds();
+
+enX_Prev =0;
+enY_Prev = 0;
+enTh_Prev = 0;
 
 while true
     myPosition = HPS.getPosition(myId);
@@ -17,11 +23,24 @@ while true
     enY = enPosition.y;
     enTh = enPosition.th; 
     
-    myX = myPosition.x;  %% + offset relating to theta
-    myY = myPosition.y; %% + offset relating to theta
-    myTh = myPosition.th;
     
-    % Here we'll just print the position out
-    disp('My position');
-    disp(myPosition);
+    myTh = myPosition.th;
+    myX = myPosition.x + .08*cos(myTh);  %% + offset relating to theta
+    myY = myPosition.y + .08*sin(myTh); %% + offset relating to theta
+    corners = getCorner(HPS);
+    
+    up = 100*sqrt((corners(1,2).y-myY)/(corners(1,2).y - corners(1,1).y));
+    down = 100*sqrt((myY-corners(1,1).y)/(corners(1,2).y - corners(1,1).y));
+    left = 100*sqrt((myX-corners(1,1).x)/(corners(2,1).x - corners(1,1).x));
+    right = 100*sqrt((corners(2,1).y-myX)/(corners(2,1).x - corners(1,1).x));
+    
+    disp(strcat('up:', int2str(up), 'down:', int2str(down)));
+    disp(strcat('left:', int2str(left), 'right', int2str(right)));
+    
+    x_diff = (enX - myX);
+    y_diff = (enY - myY); 
+    
+    
+    corners = getCorner(HPS);
+    
 end
